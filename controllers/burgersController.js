d8a414f310bebcca170b166f3dd40e6c9c196657
Luke Champ <luke.champ@outlook.com>
2018-03-23 19:42:11 -0400

    server and mysql working, need to connect to handlebars

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,8 +1,20 @@
 var burger = require ("../models/burger.js");
 const express = require('express');
 const app = express();
+var orm = require("../config/orm");
 
-app.get("/", function (req, res) {
-    
-}
-}
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/burger", function (req, res) {
+    orm.selectAll("burgers")
+    if (err){
+        return res.status(500).end();    
+    }
+    res.render("index", {burger: data});
+
+    });
+
+
+
